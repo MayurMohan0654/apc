c193330467892cf7cb036d2c84b41ec18bd897f4
MayurMohan0654 <mayurmohan2486@gmail.com>
2025-09-07 21:35:01 +0530

    handlle mysql connection and credentials for testes

--- Movie Vault/Backend/movie-vault/src/main/java/com/movievault/MovieVaultApplication.java
+++ Movie Vault/Backend/movie-vault/src/main/java/com/movievault/MovieVaultApplication.java
@@ -1,11 +1,44 @@
 package com.movievault;
-
+import java.sql.*;
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication
+// public class MovieVaultApplication {
+//     public static void main(String[] args) {
+//         SpringApplication.run(MovieVaultApplication.class, args);
+//     }
+// }
 @SpringBootApplication
 public class MovieVaultApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(MovieVaultApplication.class, args);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter MySQL username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter MySQL password: ");
+        String password = scanner.nextLine();
+
+        System.out.println("Enter database name (must exist): ");
+        String db = scanner.nextLine();
+
+        String url = "jdbc:mysql://localhost:3306/" + db;
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)){ 
+            System.out.println("Database connection successful!");
+            System.setProperty("spring.datasource.username", username);
+            System.setProperty("spring.datasource.password", password);
+
+            SpringApplication.run(MovieVaultApplication.class, args);
+
+        } catch (Exception e) {
+            System.err.println("error: " + e.getMessage());
+        }finally{
+            scanner.close();
+        }
+
     }
 }
